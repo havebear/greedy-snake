7ea49420e75850231f410243e72a94c4b25b57f5
havebear <616749285@qq.com>
2020-12-28 09:32:46 +0800

    chore: 修复边界碰撞溢出的缺陷

--- html版/game.js
+++ html版/game.js
@@ -9,14 +9,14 @@
     start () {
       const timer = setInterval(() => {
         const { snack, food } = this
-        snack.move(food)
         const { left: headX, top: headY } = snack.body[0]
         const maxX = map.offsetWidth - snack.width
         const maxY = map.offsetHeight - snack.height
+        snack.move(food)
         console.log({ maxX, maxY, headX, headY })
         if (headX < 0 || headX >= maxX || headY < 0 || headY >= maxY) {
           clearInterval(timer)
-          alert('游戏结束')
+          // alert('游戏结束')
         }
       }, 1000)
     }
